f2c5017042e11ad3fa11753b03c8b30f068912ca
Marcel Greter <marcel.greter@ocbnet.ch>
2024-06-22 15:36:50 +0200

    As you were

--- src/ast_def_macros.hpp
+++ src/ast_def_macros.hpp
@@ -134,7 +134,7 @@ private:
     } \
     public: bool operator<(const subklass& rhs) const override final { \
       auto sel = rhs.isa##klass(); \
-      return sel ? *this < *sel : this < sel; \
+      return sel ? *this < *sel : typeid(*this).before(typeid(rhs)); \
     } \
     public: bool operator==(const klass& rhs) const; \
     public: bool operator<(const klass& rhs) const; \
